f7dacd5beb3214ea7c8c5a22742b4225c3e4beef
HerryLo <13995578046@163.com>
2018-05-11 18:55:37 +0800

    保存添加文章接口

--- server/controller/api.js
+++ server/controller/api.js
@@ -3,21 +3,23 @@ import config from '../config/config'
 import {
     ArticleModel
 } from '../monoose/dbConnect'
-import path from 'path'
-import fs from 'fs'
+import {
+    CreateArtimgFs
+} from '../utils/util'
 
 class Api {
     constructor() {
         this.get = this.get.bind(this);
     }
 
-    async get(ctx, next) {
+
+    async userlist(ctx, next) {
         const {
             id,
             user
         } = ctx.state;
         if (id && user) {
-            const file = ctx.file;
+
         }
     }
 
@@ -33,36 +35,53 @@ class Api {
         } = ctx.state;
         const file = ctx.request.body.files.file;
         const data = ctx.request.body.fields;
-        const ext ç='.'+file.type.split('/')[1];
-        const name = file.name.split('.')[0]
-        const imgName = `${name}_${new Date().getTime().toString()}${ext}`;
-        const newpath =path.join(__dirname, '../public/artimg/'+imgName);
-        console.log(newpath);
-        const reader = await fs.createReadStream(file.path); //创建可读流
-        const stream = await fs.createWriteStream(newpath); //创建一个可写流
-        reader.pipe(stream);
-        ctx.body = {
-            data: {
-                imgurl: `artimg/${imgName}`
-            }
-        }
-        return;
         const result = await ArticleModel.find({
             id,
             user
         });
         if (result[0]) {
-            ctx.body = {
-                code: 1,
-                desc: '',
-                data: []
+            const imgName = await CreateArtimgFs(file);
+            const checkBool = checkArtparam(ctx);
+            if(checkBool){
+                const d = await ArticleModel.create(data);
+                if(d){
+                    ctx.body = {
+                        code: 1,
+                        desc: '添加成功',
+                        data: d
+                    }
+                }
+            }else{
+                ctx.body = {
+                    code: 1,
+                    desc: '请求参数不正确',
+                    data: []
+                }
             }
+            
         } else {
             ctx.body = {
                 code: 1,
                 desc: '用户不存在',
                 data: []
             }
+        }
+    }
+
+    /**
+     * 检查创建文章参数
+     */
+    async checkArtparam(ctx) {
+        const data = ctx.request.body.fields;
+        const d = data
+        if (d.userId &&
+            d.title &&
+            d.imgUrl &&
+            d.desc &&
+            d.tag.length > 0) {
+            return true
+        }else{
+            return  false
         }
     }
 
