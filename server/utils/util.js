b42b3abf1cd899f7ecfaf3bf29324494ee57eb48
HerryLo <13995578046@163.com>
2018-05-11 18:57:08 +0800

    保存添加文章接口

---
+++ server/utils/util.js
@@ -0,0 +1,16 @@
+import path from 'path'
+import fs from 'fs'
+
+/**
+ * 上传文件保存到服务器
+ */
+export async function CreateArtimgFs(file) {
+    const ext ='.'+file.type.split('/')[1]
+    const name = file.name.split('.')[0]
+    const imgName = `${name}_${new Date().getTime().toString()}${ext}`
+    const newpath =path.join(__dirname, '../public/artimg/'+imgName)
+    const reader = await fs.createReadStream(file.path) //创建可读流
+    const stream = await fs.createWriteStream(newpath) //创建一个可写流
+    await reader.pipe(stream)
+    return `artimg/${imgName}`
+}
